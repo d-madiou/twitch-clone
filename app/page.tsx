0ac208ae67ec36390863a0ed4e5ff198e3bd0e7c
d-madiou <madioudiallo846@gmail.com>
2025-09-10 18:54:54 +0800

    The first commit of this twitch clone

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  );
+}
