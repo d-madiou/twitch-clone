fa9c645f2e15db94fb80e5e254f7823e612359df
Thierno Madiou Diallo <madioudiallo846@gmail.com>
2025-09-25 19:27:34 +0800

    Feat: First one

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Dashboard Name</h1>
       <UserButton afterSignOutUrl="/"/>
     </div>
   );
